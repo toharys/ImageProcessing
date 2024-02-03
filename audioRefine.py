0a6f55b7966768631bfd8416d3fd4f014d9cde75
toharsimchi <toharya.simchi@mail.huji.ac.il>
2024-02-03 14:00:41 +0200

    modified filter's parameters

--- audioRefine.py
+++ audioRefine.py
@@ -19,16 +19,16 @@ def generate_spectrogram(audio_array, frame_rate) -> Tuple[np.ndarray, np.ndarra
                                                         noverlap=512)
 
     # Optionally, visualize the spectrogram
-    # plt.title('Spectrogram')
-    # plt.xlabel('Time (s)')
-    # plt.ylabel('Frequency (Hz)')
-    # plt.colorbar(label='Intensity (dB)')
-    # plt.show()
+    plt.title('Spectrogram')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Frequency (Hz)')
+    plt.colorbar(label='Intensity (dB)')
+    plt.show()
 
     return spec_values, freq_bins, time_bins
 
 
-def notch_filter(audio_array, frame_rate, target_frequency, quality_factor, end_time, start_time=0.0):
+def notch_filter(audio_array, frame_rate, target_frequency, quality_factor, end_time, start_time=0.0) -> np.ndarray:
 
     # Design notch filter
     nyquist = 0.5 * frame_rate
@@ -53,20 +53,20 @@ def save_audio_to_wav(audio_array, frame_rate, output_file):
     wavfile.write(output_file, frame_rate, audio_array)
 
 
-def q1(audio_path) -> np.array:
+def q1(audio_path) -> np.ndarray:
     audio_arr, frame_rate = wav_to_arr(audio_path)
     # frequencies, fft_result = calculate_fourier_transform(audio_arr, frame_rate)
     generate_spectrogram(audio_arr,frame_rate)
     filtered_audio = notch_filter(audio_arr, frame_rate, 1124, 4, len(audio_arr)/frame_rate)
     generate_spectrogram(filtered_audio, frame_rate)
-    save_audio_to_wav(filtered_audio, frame_rate, r"C:\Users\tohar\ImageProcessingExs\ex2\Outputs\4_q1_filtered.wav")
+    save_audio_to_wav(filtered_audio, frame_rate, r"C:\Users\tohar\ImageProcessingExs\ex2\Outputs\q1_filtered.wav")
     return filtered_audio
 
 
-def q2(audio_path) -> np.array:
+def q2(audio_path) -> np.ndarray:
     audio_arr, frame_rate = wav_to_arr(audio_path)
     generate_spectrogram(audio_arr, frame_rate)
-    filtered_audio = notch_filter(audio_arr, frame_rate, 595, 15, 4.02, 1.55)
+    filtered_audio = notch_filter(audio_arr, frame_rate, 595, 2, 4.1, 1.45)
     generate_spectrogram(filtered_audio, frame_rate)
     save_audio_to_wav(filtered_audio, frame_rate, r"C:\Users\tohar\ImageProcessingExs\ex2\Outputs\q2_filtered.wav")
     return filtered_audio
