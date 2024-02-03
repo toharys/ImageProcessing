08ee9541c1c28c990ee8603142ff7a22da9f9f96
toharsimchi <toharya.simchi@mail.huji.ac.il>
2024-02-03 13:02:49 +0200

    generic filter

--- audioRefine.py
+++ audioRefine.py
@@ -11,13 +11,7 @@ def wav_to_arr(file_path) -> Tuple[np.ndarray, int]:
     max_int16 = np.iinfo(np.int16).max
     audio_data = (audio_data / np.max(np.abs(audio_data))) * max_int16
     return audio_data.astype(np.int16), frame_rate
-    # frame_rate, audio_data = read_wav(file_path)
-    #
-    # if len(audio_data.shape) == 2:
-    #     # If stereo, take only one channel (assuming it's the same for both)
-    #     audio_data = audio_data[:, 0]
-    #
-    # return audio_data, frame_rate
+
 
 def generate_spectrogram(audio_array, frame_rate) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
 
@@ -25,42 +19,30 @@ def generate_spectrogram(audio_array, frame_rate) -> Tuple[np.ndarray, np.ndarra
                                                         noverlap=512)
 
     # Optionally, visualize the spectrogram
-    plt.title('Spectrogram')
-    plt.xlabel('Time (s)')
-    plt.ylabel('Frequency (Hz)')
-    plt.colorbar(label='Intensity (dB)')
-    plt.show()
+    # plt.title('Spectrogram')
+    # plt.xlabel('Time (s)')
+    # plt.ylabel('Frequency (Hz)')
+    # plt.colorbar(label='Intensity (dB)')
+    # plt.show()
 
     return spec_values, freq_bins, time_bins
 
-def calculate_fourier_transform(audio_array, frame_rate) -> Tuple[np.ndarray, np.ndarray]:
-    pass
-#     # Calculate the one-dimensional FFT
-#     fft_result = np.fft.fft(audio_array)  # Assuming mono audio, use audio_array[:, 0] for the first channel
-#
-#     # Calculate the frequencies corresponding to the FFT result
-#     frequencies = np.fft.fftfreq(len(fft_result), d=1/frame_rate)
-#
-#     # Optionally, visualize the result
-#     # plt.plot(frequencies, np.abs(fft_result))
-#     # plt.title('Fourier Transform of Audio')
-#     # plt.xlabel('Frequency (Hz)')
-#     # plt.ylabel('Amplitude')
-#     # plt.show()
-#
-#     return frequencies, fft_result
 
-def notch_filter(audio_array, frame_rate, target_frequency, quality_factor=30):
+def notch_filter(audio_array, frame_rate, target_frequency, quality_factor, end_time, start_time=0.0):
 
     # Design notch filter
     nyquist = 0.5 * frame_rate
     notch_frequency = target_frequency / nyquist
     b, a = iirnotch(notch_frequency, quality_factor)
-
+    start_index = int(start_time*frame_rate)
+    end_index = int(end_time*frame_rate)
     # Apply notch filter to audio array
-    filtered_audio = np.apply_along_axis(lambda x: np.convolve(x, b, mode='same'), axis=0, arr=audio_array)
+    filtered_audio = audio_array.copy()
+    filtered_audio[start_index:end_index] = np.apply_along_axis(lambda x: np.convolve(x[start_index:end_index],
+        b, mode='same'), axis=0, arr=audio_array)
 
     return filtered_audio
+
 
 def save_audio_to_wav(audio_array, frame_rate, output_file):
 
@@ -70,23 +52,26 @@ def save_audio_to_wav(audio_array, frame_rate, output_file):
     # Save the audio to a WAV file
     wavfile.write(output_file, frame_rate, audio_array)
 
+
 def q1(audio_path) -> np.array:
     audio_arr, frame_rate = wav_to_arr(audio_path)
     # frequencies, fft_result = calculate_fourier_transform(audio_arr, frame_rate)
     generate_spectrogram(audio_arr,frame_rate)
-    filtered_audio = notch_filter(audio_arr,frame_rate,1124)
+    filtered_audio = notch_filter(audio_arr, frame_rate, 1124, 4, len(audio_arr)/frame_rate)
     generate_spectrogram(filtered_audio, frame_rate)
-    save_audio_to_wav(filtered_audio, frame_rate, r"C:\Users\tohar\ImageProcessingExs\ex2\Outputs\q1_filtered.wav")
+    save_audio_to_wav(filtered_audio, frame_rate, r"C:\Users\tohar\ImageProcessingExs\ex2\Outputs\4_q1_filtered.wav")
+    return filtered_audio
 
-    # plt.plot(filtered_audio, np.abs(fft_result))
-    # plt.title('Spectrogram')
-    # plt.xlabel('Time (s)')
-    # plt.ylabel('Frequency (Hz)')
-    # plt.colorbar(label='Intensity (dB)')
-    # plt.show()
 
-# def q2(audio_path) -> np.array:
-#     audio_arr, frame_rate = wav_to_arr(audio_path)
+def q2(audio_path) -> np.array:
+    audio_arr, frame_rate = wav_to_arr(audio_path)
+    generate_spectrogram(audio_arr, frame_rate)
+    filtered_audio = notch_filter(audio_arr, frame_rate, 595, 15, 4.02, 1.55)
+    generate_spectrogram(filtered_audio, frame_rate)
+    save_audio_to_wav(filtered_audio, frame_rate, r"C:\Users\tohar\ImageProcessingExs\ex2\Outputs\q2_filtered.wav")
+    return filtered_audio
+
 
 if __name__ == "__main__":
     q1(r"C:\Users\tohar\ImageProcessingExs\ex2\Inputs\q1.wav")
+    q2(r"C:\Users\tohar\ImageProcessingExs\ex2\Inputs\q2.wav")
